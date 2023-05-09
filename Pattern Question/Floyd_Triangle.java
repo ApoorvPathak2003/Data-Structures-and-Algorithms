8624b61a978ccc9f708e093689d8fb0cc25a7890
Apoorv_Pathak <apoorvgunjanpathak@gmail.com>
2023-05-09 12:38:03 +0530

    Add files via upload

---
+++ Pattern Question/Floyd_Triangle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Floyd_Triangle{
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of iterations: ");
+        int n = input.nextInt();
+
+        int number = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(number+" ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
