8c9f2b30b9a56b192740747ea590af87efb11437
Apoorv_Pathak <apoorvgunjanpathak@gmail.com>
2023-06-27 16:21:49 +0530

    Add files via upload

---
+++ BinaryTree/PostOrder_Traversal.java
@@ -0,0 +1,47 @@
+package BinaryTree;
+
+public class PostOrder_Traversal {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    static class binary_tree{
+        static int index = -1;
+        public Node create_binary_tree(int nodes[]) {
+            index++;
+            if (nodes[index] == -1) {
+                return null;
+            }
+
+            Node new_node = new Node(nodes[index]);
+            new_node.left = create_binary_tree(nodes);
+            new_node.right = create_binary_tree(nodes);
+
+            return new_node;
+        }
+    }
+    public static void post_order_traversal(Node root){
+        // Left SubTree -> Right SubTree -> Root
+        if (root == null) {
+            return;
+        }
+        post_order_traversal(root.left);
+        post_order_traversal(root.right);
+        System.out.print(root.data + " ");
+    }
+    public static void main(String[] args) {
+        int nodes[] = {2, 5, -1, 0, -1, -1, 9, 6, -1, -1, -1};
+        binary_tree tree = new binary_tree();
+        Node root = tree.create_binary_tree(nodes);
+
+        System.out.print("Post-Order Traversal: ");
+        post_order_traversal(root);
+    }
+}
